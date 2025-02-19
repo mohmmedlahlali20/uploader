422591516c7049bb3439096fd7f91f932aad3ba2
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2025-02-19 20:58:14 +0100

    feat(middleware): improve upload middleware handling

--- uploadMiddleware.js
+++ uploadMiddleware.js
@@ -16,7 +16,7 @@ const fileFilter = (req, file, cb) => {
 const upload = multer({
   storage,
   fileFilter,
-  limits: { fileSize: 5 * 1024 * 1024 }, 
+  // limits: { fileSize: 5 * 1024 * 1024 }, 
 });
 
 module.exports = upload;
