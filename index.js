a746ef2eb9379d840ab719265bc3b8e8c7eae464
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2025-02-28 16:41:02 +0000

    chore(config): update main index file

--- index.js
+++ index.js
@@ -8,21 +8,21 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors());
-app.post('/upload/avatar', upload.single('avatar'), async (req, res) => {
-    
-    
-  try {
-    if (!req.file) {
-      return res.status(400).json({ error: 'No file uploaded' });
+  app.post('/upload', upload.single('avatar'), async (req, res) => {
+      
+      
+    try {
+      if (!req.file) {
+        return res.status(400).json({ error: 'No file uploaded' });
+      }
+      
+      const imageUrl = await uploadToMinIO(req.file);
+      console.log(imageUrl);
+      
+      return res.status(200).json({ imageUrl });
+    } catch (error) {
+      return res.status(500).json({ error: error.message });
     }
-     
-    const imageUrl = await uploadToMinIO(req.file);
-    console.log(imageUrl);
-    
-    res.json({ imageUrl });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
+  });
 
 app.listen(4000, () => console.log('Server running on port 4000'));
